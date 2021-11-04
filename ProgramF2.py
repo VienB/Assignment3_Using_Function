10d7ff1a2f65ad4075ca9a83d27b21eecaa2c40c
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-04 01:16:13 -0700

    attempt

--- ProgramF2.py
+++ ProgramF2.py
@@ -1,9 +1,9 @@
 # Create a program that will ask how many apples and oranges you want to buy.
-# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display th.e total amount you need to pay if apple price is 20 pesos and orange is 25.
 # Display the output in the following format.
-# The total amount is ______.
+# The total amount is ______
 
-# use a function
+# make a function
 
 def GetApple():
     AppleF = int(input("How many apple you want to buy? "))
@@ -13,14 +13,20 @@ def GetOrange():
     OrangeF = int(input("How many apple you want to buy? "))
     return OrangeF
 
-def ApplePrice():
-    Aprice = int(apple * 20) 
-    return
+def Calculate():
+    price = (apples * 20) + (oranges * 25)
+    return price
 
-def OrangePrice():
-    Oprice = int(orange * 25)
-    return Oprice
+def OutPutF(Fapple, Forange1, Fcal):
+    print(f"The total amount is {cal}.")
 
-apple = GetApple()
-orange = GetOrange()
+#call all the functions
 
+apples = GetApple()
+oranges = GetOrange()
+cal = Calculate()
+OutPutF(apples, oranges, cal)
+
+# short conclusion:
+# I conclude that it is important to understand the logic of the program because one mistake can affect
+# all the lines. But it is okay to make a mistake considering that it will give you a learnings and realization.
