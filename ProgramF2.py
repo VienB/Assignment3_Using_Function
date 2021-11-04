b50ee95145cd5b0cb804efc7c0fceb0993d17ca6
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-04 00:03:33 -0700

    attempt

---
+++ ProgramF2.py
@@ -0,0 +1,26 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output in the following format.
+# The total amount is ______.
+
+# use a function
+
+def GetApple():
+    AppleF = int(input("How many apple you want to buy? "))
+    return AppleF
+    
+def GetOrange():
+    OrangeF = int(input("How many apple you want to buy? "))
+    return OrangeF
+
+def ApplePrice():
+    Aprice = int(apple * 20) 
+    return
+
+def OrangePrice():
+    Oprice = int(orange * 25)
+    return Oprice
+
+apple = GetApple()
+orange = GetOrange()
+
